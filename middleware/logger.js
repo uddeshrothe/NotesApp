2f768fd175f2071fbc3538dc47175aaa6a89cb4b
uddeshrothe <uddesh.rothe1@gmail.com>
2022-11-30 11:47:25 +0530

    updated server.js

---
+++ middleware/logger.js
@@ -0,0 +1,2 @@
+const { format } = require('date-fns');
+const { v4: uuid } = require('uuid');
--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 4000;
 
-app.use('/', express.static(path.join(__dirname, '/public')));
+app.use(express.json())
+
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes/root'))
 
